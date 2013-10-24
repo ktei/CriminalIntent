954a5d0dc432326b64adb45927ed4244d4db9599
Rui Cheng <ktei2008@gmail.com>
2013-10-24 20:18:58 +1100

    initial commit

---
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class CrimeActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+
+}
